e79e36f15df64b9c2ab2d4738cbdf18443d26386
guerric-k <160793947+guerric-k@users.noreply.github.com>
2025-03-07 03:17:51 +0000

    add config vars

--- env.py
+++ env.py
@@ -2,3 +2,6 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_pyjJhLQzS14b@ep-holy-water-a2s79pp7.eu-central-1.aws.neon.tech/twins_scuff_sax_189549")
+
+os.environ.setdefault(
+    "SECRET_KEY", "+0zrkul%03jb1)mf1w%^94l-f(0e!c6b3rwdbvq@v0qhgl5lbz")
